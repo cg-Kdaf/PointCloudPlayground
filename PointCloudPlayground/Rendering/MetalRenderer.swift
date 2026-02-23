c29b25aa0f696996aa96fa99566dd7f5e3ff2b7b
Colin Marmond <kdblender@gmail.com>
2026-02-23 16:42:37 +0100

    Added a basic gizmo and remove unsused cube

--- PointCloudPlayground/Rendering/MetalRenderer.swift
+++ PointCloudPlayground/Rendering/MetalRenderer.swift
@@ -2,11 +2,18 @@ import Foundation
 import Metal
 import MetalKit
 
+private struct MeshVertex {
+  var position: SIMD3<Float>
+  var color: SIMD3<Float>
+}
+
 final class MetalRenderer: NSObject, MTKViewDelegate {
   private let commandQueue: MTLCommandQueue
-  private let pipelineState: MTLRenderPipelineState
-  private let depthStencilState: MTLDepthStencilState
-  private let cubeMesh: CubeMesh
+  private let pointPipelineState: MTLRenderPipelineState
+  private let coloredPipelineState: MTLRenderPipelineState
+  private let sceneDepthStencilState: MTLDepthStencilState
+  private let gizmoVertexBuffer: MTLBuffer
+  private let gizmoVertexCount: Int
   private let cameraBuffer: MTLBuffer
   private let orbitCamera: OrbitCamera
   private let loader: PointCloudLoader = .init()
@@ -29,31 +36,53 @@ final class MetalRenderer: NSObject, MTKViewDelegate {
     }
     
     guard let library = device.makeDefaultLibrary(),
-          let vertexFunction = library.makeFunction(name: "basic_vertex"),
+          let pointVertexFunction = library.makeFunction(name: "point_vertex"),
+          let coloredVertexFunction = library.makeFunction(name: "colored_vertex"),
           let fragmentFunction = library.makeFunction(name: "basic_fragment") else {
       return nil
     }
     
-    let pipelineDescriptor = MTLRenderPipelineDescriptor()
-    pipelineDescriptor.vertexFunction = vertexFunction
-    pipelineDescriptor.fragmentFunction = fragmentFunction
-    pipelineDescriptor.colorAttachments[0].pixelFormat = mtkView.colorPixelFormat
-    pipelineDescriptor.depthAttachmentPixelFormat = mtkView.depthStencilPixelFormat
-    
-    guard let pipelineState = try? device.makeRenderPipelineState(descriptor: pipelineDescriptor) else {
+    let pointPipelineDescriptor = MTLRenderPipelineDescriptor()
+    pointPipelineDescriptor.vertexFunction = pointVertexFunction
+    pointPipelineDescriptor.fragmentFunction = fragmentFunction
+    pointPipelineDescriptor.colorAttachments[0].pixelFormat = mtkView.colorPixelFormat
+    pointPipelineDescriptor.depthAttachmentPixelFormat = mtkView.depthStencilPixelFormat
+
+    guard let pointPipelineState = try? device.makeRenderPipelineState(descriptor: pointPipelineDescriptor) else {
+      return nil
+    }
+
+    let coloredPipelineDescriptor = MTLRenderPipelineDescriptor()
+    coloredPipelineDescriptor.vertexFunction = coloredVertexFunction
+    coloredPipelineDescriptor.fragmentFunction = fragmentFunction
+    coloredPipelineDescriptor.colorAttachments[0].pixelFormat = mtkView.colorPixelFormat
+    coloredPipelineDescriptor.depthAttachmentPixelFormat = mtkView.depthStencilPixelFormat
+    coloredPipelineDescriptor.vertexDescriptor = Self.makeColoredVertexDescriptor()
+
+    guard let coloredPipelineState = try? device.makeRenderPipelineState(descriptor: coloredPipelineDescriptor) else {
       return nil
     }
     
-    let depthDescriptor = MTLDepthStencilDescriptor()
-    depthDescriptor.depthCompareFunction = .less
-    depthDescriptor.isDepthWriteEnabled = true
-    
-    guard let depthStencilState = device.makeDepthStencilState(descriptor: depthDescriptor) else {
+    let sceneDepthDescriptor = MTLDepthStencilDescriptor()
+    sceneDepthDescriptor.depthCompareFunction = .less
+    sceneDepthDescriptor.isDepthWriteEnabled = true
+
+    guard let sceneDepthStencilState = device.makeDepthStencilState(descriptor: sceneDepthDescriptor) else {
       return nil
     }
+
+    let gizmoDepthDescriptor = MTLDepthStencilDescriptor()
+    gizmoDepthDescriptor.depthCompareFunction = .less
+    gizmoDepthDescriptor.isDepthWriteEnabled = false
     
-    guard let cubeMesh = CubeMesh(device: device),
-          let cameraBuffer = device.makeBuffer(length: MemoryLayout<CameraUniforms>.stride) else {
+    guard let cameraBuffer = device.makeBuffer(length: MemoryLayout<CameraUniforms>.stride) else {
+      return nil
+    }
+
+    let gizmoVertices = Self.makeGizmoVertices()
+    guard let gizmoVertexBuffer = device.makeBuffer(bytes: gizmoVertices,
+                                                    length: MemoryLayout<MeshVertex>.stride * gizmoVertices.count,
+                                                    options: .storageModeShared) else {
       return nil
     }
     
@@ -64,12 +93,39 @@ final class MetalRenderer: NSObject, MTKViewDelegate {
     let orbitCamera = OrbitCamera(aspectRatio: initialAspectRatio)
     
     self.commandQueue = commandQueue
-    self.pipelineState = pipelineState
-    self.depthStencilState = depthStencilState
-    self.cubeMesh = cubeMesh
+    self.pointPipelineState = pointPipelineState
+    self.coloredPipelineState = coloredPipelineState
+    self.sceneDepthStencilState = sceneDepthStencilState
+    self.gizmoVertexBuffer = gizmoVertexBuffer
+    self.gizmoVertexCount = gizmoVertices.count
     self.cameraBuffer = cameraBuffer
     self.orbitCamera = orbitCamera
     super.init()
+  }
+
+  private static func makeColoredVertexDescriptor() -> MTLVertexDescriptor {
+    let descriptor = MTLVertexDescriptor()
+    descriptor.attributes[0].format = .float3
+    descriptor.attributes[0].offset = 0
+    descriptor.attributes[0].bufferIndex = 0
+    descriptor.attributes[1].format = .float3
+    descriptor.attributes[1].offset = MemoryLayout<SIMD3<Float>>.stride
+    descriptor.attributes[1].bufferIndex = 0
+    descriptor.layouts[0].stride = MemoryLayout<MeshVertex>.stride
+    descriptor.layouts[0].stepFunction = .perVertex
+    return descriptor
+  }
+
+  private static func makeGizmoVertices() -> [MeshVertex] {
+    let axisExtent: Float = 1_000_000
+    return [
+      MeshVertex(position: SIMD3<Float>(-axisExtent, 0, 0), color: SIMD3<Float>(1, 0, 0)),
+      MeshVertex(position: SIMD3<Float>( axisExtent, 0, 0), color: SIMD3<Float>(1, 0, 0)),
+      MeshVertex(position: SIMD3<Float>(0, -axisExtent, 0), color: SIMD3<Float>(0, 1, 0)),
+      MeshVertex(position: SIMD3<Float>(0,  axisExtent, 0), color: SIMD3<Float>(0, 1, 0)),
+      MeshVertex(position: SIMD3<Float>(0, 0, -axisExtent), color: SIMD3<Float>(0, 0, 1)),
+      MeshVertex(position: SIMD3<Float>(0, 0,  axisExtent), color: SIMD3<Float>(0, 0, 1))
+    ]
   }
   
   func orbit(deltaX: Float, deltaY: Float) {
@@ -92,9 +148,13 @@ final class MetalRenderer: NSObject, MTKViewDelegate {
     pointCloudData = loader.loadLazFile(at: filepath)
     
     guard let pointCloudData else { return }
-    
-    pointCloudData.buffer.withUnsafeBytes {
-      pointCloudBuffer = commandQueue.device.makeBuffer(bytes: $0,
+
+    pointCloudData.buffer.withUnsafeBytes { (rawBuffer: UnsafeRawBufferPointer) in
+      guard let baseAddress = rawBuffer.baseAddress else {
+        pointCloudBuffer = nil
+        return
+      }
+      pointCloudBuffer = commandQueue.device.makeBuffer(bytes: baseAddress,
                                                         length: pointCloudData.buffer.count,
                                                         options: .storageModeShared)
     }
@@ -114,29 +174,24 @@ final class MetalRenderer: NSObject, MTKViewDelegate {
     orbitCamera.updateOrbit()
     var cameraUniforms = orbitCamera.makeUniforms()
     cameraBuffer.contents().copyMemory(from: &cameraUniforms, byteCount: MemoryLayout<CameraUniforms>.stride)
-    
+
+    renderEncoder.setRenderPipelineState(coloredPipelineState)
+    renderEncoder.setDepthStencilState(sceneDepthStencilState)
+    renderEncoder.setCullMode(.none)
+    renderEncoder.setVertexBuffer(gizmoVertexBuffer, offset: 0, index: 0)
+    renderEncoder.setVertexBuffer(cameraBuffer, offset: 0, index: 1)
+    renderEncoder.drawPrimitives(type: .line, vertexStart: 0, vertexCount: gizmoVertexCount)
+
     if let pointCloudBuffer, let pointCloudData {
-      renderEncoder.setRenderPipelineState(pipelineState)
-      renderEncoder.setDepthStencilState(depthStencilState)
+      renderEncoder.setRenderPipelineState(pointPipelineState)
+      renderEncoder.setDepthStencilState(sceneDepthStencilState)
       // Pass the buffer we created from NSData
       renderEncoder.setVertexBuffer(pointCloudBuffer, offset: 0, index: 0)
       renderEncoder.setVertexBuffer(cameraBuffer, offset: 0, index: 1)
       renderEncoder.drawPrimitives(type: .point, vertexStart: 0, vertexCount: Int(pointCloudData.pointCount))
-      renderEncoder.endEncoding()
-    } else {
-      renderEncoder.setRenderPipelineState(pipelineState)
-      renderEncoder.setDepthStencilState(depthStencilState)
-      renderEncoder.setCullMode(.back)
-      renderEncoder.setFrontFacing(.counterClockwise)
-      renderEncoder.setVertexBuffer(cubeMesh.vertexBuffer, offset: 0, index: 0)
-      renderEncoder.setVertexBuffer(cameraBuffer, offset: 0, index: 1)
-      renderEncoder.drawIndexedPrimitives(type: .triangle,
-                                          indexCount: cubeMesh.indexCount,
-                                          indexType: .uint16,
-                                          indexBuffer: cubeMesh.indexBuffer,
-                                          indexBufferOffset: 0)
-      renderEncoder.endEncoding()
     }
+
+    renderEncoder.endEncoding()
     
     commandBuffer.present(drawable)
     commandBuffer.commit()
--- PointCloudPlayground/Rendering/Shaders.metal
+++ PointCloudPlayground/Rendering/Shaders.metal
@@ -1,27 +1,22 @@
 #include <metal_stdlib>
 using namespace metal;
 
-struct VertexIn {
-  float3 position;
-  //  float3 color;
+struct ColoredVertexIn {
+  float3 position [[attribute(0)]];
+  float3 color [[attribute(1)]];
 };
 
 struct CameraUniforms {
   float4x4 viewProjectionMatrix;
 };
 
-//struct VertexOut {
-//  float4 position [[position]];
-//  float3 color;
-//};
-
 struct VertexOut {
   float4 position [[position]];
-  float point_size [[point_size]]; // Tells Metal how big the dot should be
+  float point_size [[point_size]];
   float4 color;
 };
 
-vertex VertexOut basic_vertex(const device float3 *vertices [[buffer(0)]],
+vertex VertexOut point_vertex(const device float3 *vertices [[buffer(0)]],
                               constant CameraUniforms &camera [[buffer(1)]],
                               uint vertexID [[vertex_id]]) {
   VertexOut out;
@@ -29,6 +24,16 @@ vertex VertexOut basic_vertex(const device float3 *vertices [[buffer(0)]],
   out.position = camera.viewProjectionMatrix * worldPosition;
   out.color = float4(float3(worldPosition.y / 80.0 + 0.5), 1.0);
   out.point_size = 600.0 / (length(out.position) + 0.01);
+  return out;
+}
+
+vertex VertexOut colored_vertex(ColoredVertexIn in [[stage_in]],
+                                constant CameraUniforms &camera [[buffer(1)]]) {
+  VertexOut out;
+  float4 worldPosition = float4(in.position, 1.0);
+  out.position = camera.viewProjectionMatrix * worldPosition;
+  out.color = float4(in.color, 1.0);
+  out.point_size = 1.0;
   return out;
 }
 
